52c6ad2b3b7fd4b9a24bce4448673ecef8431609
Angusyg <gael.clair@gmail.com>
2018-04-14 02:22:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res) {
-  res.send('Hello World NEW! XXXXXXXXXXXXXAAAAAAAAAAAAAAAAAAAAAA')
+  res.send('Hello World NEW! WW')
 })
 
 app.listen(process.env.PORT, function() {
